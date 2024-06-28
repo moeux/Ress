d3a3c8ed49ae1951adb53848db63716a8c94db74
moeux <git@moeux.de>
2024-06-29 01:09:17 +0200

    Switched to webhook approach and added working message sending example.

--- Ress/Program.cs
+++ Ress/Program.cs
@@ -3,55 +3,57 @@ using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using Discord;
-using Discord.WebSocket;
+using Discord.Webhook;
 
 namespace Ress;
 
 internal static class Program
 {
+    private static DiscordWebhookClient _client;
+
     private static async Task Main()
     {
+        _client = new DiscordWebhookClient(Environment.GetEnvironmentVariable("RESS_WEBHOOK_URI"));
+
+        _client.Log += message =>
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        };
+
+        await _client.SendMessageAsync(embeds: CreateMessage());
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    private static List<Embed> CreateMessage()
+    {
+        var embeds = new List<Embed>();
         var xmlReader = XmlReader.Create("https://www.netcup-sonderangebote.de/feed");
         var syndicationFeed = SyndicationFeed.Load(xmlReader);
 
         foreach (var item in syndicationFeed.Items)
         {
-            var title = (item.Title?.Text ?? "No Title.").ToMarkdown();
             var descriptionBuilder = new StringBuilder();
 
             foreach (var extension in item.ElementExtensions)
             {
                 var element = extension.GetObject<XElement>();
+
                 if (element.Name.LocalName == "encoded" && element.Name.Namespace.ToString().Contains("content"))
-                    descriptionBuilder.Append(element.Value.ToMarkdown());
+                    descriptionBuilder.Append(element.Value);
             }
 
-            await SendMessageAsync(title, descriptionBuilder.ToString());
-            break;
+            embeds.Add(new EmbedBuilder
+            {
+                Title = (item.Title?.Text ?? "No Title.").ToMarkdown(),
+                Description = descriptionBuilder.ToString().ToMarkdown(),
+                Url = item.Links.FirstOrDefault()?.Uri?.ToString(),
+                Timestamp = item.PublishDate,
+                Color = new Color(4, 99, 115),
+                Footer = new EmbedFooterBuilder().WithText(item.Categories.FirstOrDefault()?.Name ?? "")
+            }.Build());
         }
-    }
 
-    private static async Task SendMessageAsync(string title, string description)
-    {
-        var client = new DiscordSocketClient();
-        var token = Environment.GetEnvironmentVariable("RESS_DISCORD_BOT_TOKEN");
-        var embed = new EmbedBuilder
-        {
-            Title = title,
-            Description = description,
-            Timestamp = DateTimeOffset.Now,
-            Color = new Color(4, 99, 115)
-        }.Build();
-
-        client.Log += message =>
-        {
-            Console.WriteLine(message);
-            return Task.CompletedTask;
-        };
-
-        await client.LoginAsync(TokenType.Bot, token);
-        await client.StartAsync();
-        await client.GetGuild(950625232446701578).GetTextChannel(1256246245396058202).SendMessageAsync(embed: embed);
-        await client.LogoutAsync();
+        return embeds;
     }
 }
