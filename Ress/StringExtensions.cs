f51e224beebd04bf5a8a9bae4d02f02a0cb2f5c5
moeux <git@moeux.de>
2024-06-28 23:30:46 +0200

    Added string extensions.

---
+++ Ress/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ReverseMarkdown;
+
+namespace Ress;
+
+public static class StringExtensions
+{
+    private static readonly Converter Converter = new(new Config
+    {
+        UnknownTags = Config.UnknownTagsOption.Bypass,
+        RemoveComments = true,
+        SmartHrefHandling = true
+    });
+
+    public static string ToMarkdown(this string str)
+    {
+        return Converter.Convert(str);
+    }
+}
