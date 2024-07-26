ff080800201c560aa66837760d2995dc38ac8cf0
moeux <git@moeux.de>
2024-07-26 02:11:35 +0200

    Added a timer for continuous feed update checking and added logic to only sent new items to the channel.

--- Ress/Program.cs
+++ Ress/Program.cs
@@ -1,38 +1,82 @@
 ﻿using System.ServiceModel.Syndication;
 using System.Text;
+using System.Timers;
 using System.Xml;
 using System.Xml.Linq;
 using Discord;
 using Discord.Webhook;
+using Timer = System.Timers.Timer;
 
 namespace Ress;
 
 internal static class Program
 {
-    private static DiscordWebhookClient _client;
+    private static string _feedUri;
+    private static DiscordWebhookClient _webhookClient;
+    private static Timer _timer;
+    private static DateTimeOffset _lastUpdatedTime;
 
     private static async Task Main()
     {
-        _client = new DiscordWebhookClient(Environment.GetEnvironmentVariable("RESS_WEBHOOK_URI"));
+        _feedUri = Environment.GetEnvironmentVariable("RESS_FEED_URI");
 
-        _client.Log += message =>
+        _webhookClient = new DiscordWebhookClient(Environment.GetEnvironmentVariable("RESS_WEBHOOK_URI"));
+        _webhookClient.Log += message =>
         {
             Console.WriteLine(message.ToString());
             return Task.CompletedTask;
         };
 
-        await _client.SendMessageAsync(embeds: CreateMessage());
+        _timer = new Timer(TimeSpan.FromSeconds(5));
+        _timer.Elapsed += UpdateFeed;
+        _timer.Start();
+
+        _lastUpdatedTime = DateTimeOffset.MinValue;
+
+        Console.WriteLine("Client initialized.\nFeed URI: {0}\nTimer Interval: {1}",
+            _feedUri,
+            TimeSpan.FromMilliseconds(_timer.Interval).ToString());
+
         await Task.Delay(Timeout.Infinite);
     }
 
-    private static List<Embed> CreateMessage()
+    private static void UpdateFeed(object? sender, ElapsedEventArgs e)
+    {
+        var xmlReader = XmlReader.Create(_feedUri);
+        var syndicationFeed = SyndicationFeed.Load(xmlReader);
+        var lastUpdatedTime = syndicationFeed.LastUpdatedTime;
+
+        if (_lastUpdatedTime.CompareTo(lastUpdatedTime) >= 0) return;
+
+        Console.WriteLine("Detected a feed update.");
+        SendMessage(syndicationFeed);
+
+        _lastUpdatedTime = lastUpdatedTime;
+    }
+
+    private static void SendMessage(SyndicationFeed syndicationFeed)
+    {
+        var embeds = CreateMessage(syndicationFeed);
+
+        if (embeds.Count != 0)
+        {
+            _webhookClient.SendMessageAsync(embeds: embeds);
+            Console.WriteLine($"Sent {embeds.Count} new items.");
+        }
+        else
+        {
+            Console.WriteLine("Sent no new items.");
+        }
+    }
+
+    private static List<Embed> CreateMessage(SyndicationFeed syndicationFeed)
     {
         var embeds = new List<Embed>();
-        var xmlReader = XmlReader.Create("https://www.netcup-sonderangebote.de/feed");
-        var syndicationFeed = SyndicationFeed.Load(xmlReader);
 
         foreach (var item in syndicationFeed.Items)
         {
+            if (_lastUpdatedTime.CompareTo(item.PublishDate) >= 0) continue;
+
             var descriptionBuilder = new StringBuilder();
 
             foreach (var extension in item.ElementExtensions)
