a609828b38c473fb75285c382379d32a8fb190bf
moeux <git@moeux.de>
2024-09-04 23:40:20 +0200

    Added embed limit constants.

---
+++ Ress/EmbedLimit.cs
@@ -0,0 +1,10 @@
+namespace Ress;
+
+public static class EmbedLimit
+{
+    public const int Title = 256;
+    public const int Description = 4096;
+    public const int Footer = 2048;
+    public const int Count = 10;
+    public const int Total = 6000;
+}
